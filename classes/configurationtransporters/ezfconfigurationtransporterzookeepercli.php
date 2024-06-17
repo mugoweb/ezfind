d9e4fea10f797c07c1054e6de3fa9ceb3baf0f21
Philipp Kamps <pkamps@mugo.ca>
2024-06-17 09:58:11 +0200

    Add configurable file path for Zookeeper CLI configuration transporter
    The changes allow flexibility when setting the configuration file path for the eZFConfigurationTransporterZookeeperCli class. Previously, the path was hardcoded into the class, but now it can be set via ConfigurationTransporterParameters in solr.ini.

--- classes/configurationtransporters/ezfconfigurationtransporterzookeepercli.php
+++ classes/configurationtransporters/ezfconfigurationtransporterzookeepercli.php
@@ -3,16 +3,19 @@
 class eZFConfigurationTransporterZookeeperCli extends ezfConfigurationTransporter
 {
     protected $cli;
+    protected $configFilePath;
 
     public function __construct( $parameters )
     {
         $this->cli = $parameters[ 'cli' ] ?? '/opt/zookeeper/bin/zkCli.sh';
+        $this->configFilePath = $parameters[ 'ConfigFilePath' ] ?? '/configs/ezp_collection_config/elevate.xml';
+
         parent::__construct( $parameters );
     }
 
     public function push( $configuration )
     {
-        exec($this->cli . ' set /configs/ezp_collection_config/elevate.xml ' . escapeshellarg( $configuration ), $output, $exitCode );
+        exec($this->cli . ' set '. $this->configFilePath . ' ' . escapeshellarg( $configuration ), $output, $exitCode );
 
         return $exitCode === 0;
     }
